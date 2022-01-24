9f4068b6b98bdcf30eae8bea701f0e691489385f
unknown <roguerifle@gmail.com>
2022-01-25 00:36:58 +0900

    ready to add astar

--- main.cpp
+++ main.cpp
@@ -3,47 +3,68 @@
 #include <algorithm>
 #include <vector>
 #include <assert.h>
+#include <queue>
+
+struct v2
+{
+    v2() : v2(0, 0){};
+    v2(int in_x, int in_y) : x(in_x), y(in_y){};
+    int x;
+    int y;
+
+	v2 operator-() const
+    {
+        return v2(-x, -y);
+    };
+	v2 operator+(const v2& rh) const
+    {
+        return v2(x + rh.x, y + rh.y);
+    };
+	v2 operator-(const v2& rh) const
+    {
+        return v2(x - rh.x, y - rh.y);
+    };
+};
 
 class Tile
 {
 public:
-    Tile():Tile(0, 0) {}
-    Tile(int in_x, int in_y) : x(in_x), y(in_y) {}
+    Tile():Tile(v2(0, 0)) {}
+    Tile(v2 in_pos) : pos(in_pos) {}
     char character = '.';
-    bool is_border(int width, int height) 
+    bool is_border(v2 size) 
     {
-        if (x == 0 || x == (width-1) )
+        if (pos.x == 0 || pos.x == (size.x-1) )
             return true;
-        if (y == 0 || y == (height-1) )
+        if (pos.y == 0 || pos.y == (size.y-1) )
             return true;
 
         return false;
     }
 
-    int x = 0;
-    int y = 0;
+    v2 pos;
 };
 
 class Map
 {
 public:
     Map() = delete;
-    Map(int size_x, int size_y) : width(size_x), height(size_y), tiles(size_x * size_y) 
+    Map(v2 in_size) : size(in_size), tiles(in_size.x * in_size.y) 
     {
         size_t tile_num = tiles.size();
         for (size_t i = 0; i < tile_num; i++)
         {
-            tiles[i].x = static_cast<int>(i%width);
-            tiles[i].y = static_cast<int>(i/width);
+            tiles[i].pos.x = static_cast<int>(i%size.x);
+            tiles[i].pos.y = static_cast<int>(i/size.x);
         }
     }
 
     Tile& get_tile(const int x, const int y)
     { 
-        assert(width >= x);
-        assert(height >= y);
+        assert(size.x >= x);
+        assert(size.x >= y);
 
-        return tiles[x + width*y];
+        return tiles[x + size.x*y];
     };
 
     void print()
@@ -52,7 +73,7 @@ public:
         for (size_t i = 0; i < tile_num; i++)
         {
             std::cout << tiles[i].character;
-            if ((i+1) % width == 0)
+            if ((i+1) % size.x == 0)
                 std::cout << '\n';
         }
     }
@@ -61,49 +82,64 @@ public:
     {
         for (Tile& tile : tiles)
         {
-            if (tile.is_border(width, height))
+            if (tile.is_border(size))
                 tile.character = '#';
         }
     }
 
     std::vector<Tile> tiles;
 
-    int width;
-    int height;
+    v2 size;
 };
 
 struct Room
 {
-    int cp_x;
-    int cp_y;
-    int width;
-    int height;
+    v2 pos;
+    v2 size;
 
-    void clamp_size(int map_width, int map_height)
+    void clamp_size(v2 map_size)
     {
-        width = std::clamp(cp_x + width, 0, map_width-1);
-        height = std::clamp(cp_y + height, 0, map_height-1);
+        size.x = std::clamp(pos.x + size.x, 0, map_size.x-1);
+        size.y = std::clamp(pos.y + size.y, 0, map_size.y-1);
+    }
+};
+
+class AStar
+{
+public:
+    AStar(Map& in_map) : map(in_map) {}
+    Map& map;
+
+    std::priority_queue<Room, std::vector<Room>> set;
+    
+    void draw_way(v2 start, v2 end)
+    {
+
     }
 };
 
 class BSPDungeon
 {
-    BSPDungeon(Map& map);
+    BSPDungeon(Map& in_map) : map(in_map) {}
+    Map& map;
+    std::vector<Room> rooms;
+    std::vector<std::shared_ptr<Tile>> walkable_floors;
+
     void Generate()
     {
 
     };
-    std::vector<Room> rooms;
-    std::vector<std::shared_ptr<Tile>> walkable_floors;
 };
 
-int main(int, char**) {
+int main(int, char**) 
+{
 
-    Map map(80,25);
-
+    Map map(v2(80,25));
     map.draw_border();
 
-    map.get_tile(79, 24).character = '$';
+    AStar astar(map);
+    astar.draw_way(v2(0, 0), v2(79, 24));
+    
     map.print();
 
     return 0;
